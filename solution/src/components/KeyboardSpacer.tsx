61895b924fc231ce59b6c06933b4d63babcfd0c3
brantalikp <brantalikp@gmail.com>
2025-04-18 13:26:39 +0200

    keyboard fix

--- solution/src/components/KeyboardSpacer.tsx
+++ solution/src/components/KeyboardSpacer.tsx
@@ -1,8 +1,13 @@
 import { useKeyboardHeight } from "~/hooks/useKeyboardHeight";
 import Animated from "react-native-reanimated";
 import React from "react";
+import { Platform } from "react-native";
 
 export const KeyboardSpacer = () => {
   const { keyboardHeight } = useKeyboardHeight();
-  return <Animated.View style={keyboardHeight} />;
+  return Platform.OS === "ios" ? (
+    <Animated.View style={keyboardHeight} />
+  ) : (
+    <></>
+  );
 };

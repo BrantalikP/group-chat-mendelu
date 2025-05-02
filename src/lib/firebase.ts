6fe02ae7f501bccf78bc68224ac219760a3f06c6
brantalikp <brantalikp@gmail.com>
2025-05-02 09:17:29 +0200

    firebase

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyBu7KWN3pdEJl7OS2GB5V0cnCyp4Rm2GSQ",
+  authDomain: "svetik-10007.firebaseapp.com",
+  projectId: "svetik-10007",
+  storageBucket: "svetik-10007.firebasestorage.app",
+  messagingSenderId: "219006782323",
+  appId: "1:219006782323:web:702ad271f324c8a3dd5306",
 };
 
 const app = initializeApp(firebaseConfig);

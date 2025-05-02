fc1ba8d7af349f9540cb54933fb72af9ce942b7e
brantalikp <brantalikp@gmail.com>
2025-05-02 09:42:57 +0200

    firebase

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyC3ypColfv9tQ05c4QAyHTlcxZyrFTjbw4",
+  authDomain: "mendelu-group-2.firebaseapp.com",
+  projectId: "mendelu-group-2",
+  storageBucket: "mendelu-group-2.firebasestorage.app",
+  messagingSenderId: "308583334673",
+  appId: "1:308583334673:web:40d3b2730292583fbfc2b0",
 };
 
 const app = initializeApp(firebaseConfig);
